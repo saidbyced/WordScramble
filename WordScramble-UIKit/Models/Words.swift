9f83c523610e6cdc7d3faf7e2a3a3662da6311af
Chris Eadie <chris.eadie@outlook.com>
2020-10-06 13:22:12 +0100

    Add current word for better encapsulation

--- WordScramble-UIKit/Models/Words.swift
+++ WordScramble-UIKit/Models/Words.swift
@@ -9,13 +9,23 @@ import Foundation
 
 struct Words {
     var all: [String] = ["silkworm"]
+    var current: String = "silkworm"
     var used = [String]()
     
     init() {
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
-                self.all = startWords.components(separatedBy: "\n")
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                self.all = allWords
+                self.current = allWords.randomElement()!.lowercased()
             }
+        }
+    }
+    
+    mutating func nextCurrent() {
+        if let new = all.randomElement()?.lowercased() {
+            current = new
         }
     }
 }
